8f9dcccf4f0154ed73ceb6aef234bb413b8d6835
Ross Brown <rbrownwsws@googlemail.com>
2026-07-29 22:05:52 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# rbrownwsws/attest-melange-provenance
+
+Attests the [melange](https://github.com/chainguard-dev/melange) generated build provenance of a built package.
+
+> **N.B.**
+> 
+> This action is intended to be used with the [rbrownwsws/release-melange-package](https://github.com/rbrownwsws/release-melange-package) action.
+>
+> It will not work with the raw `*.attest.tar.gz` files that melange outputs.
---
+++ prepare.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if [[ ! -f "${ATTESTATION_FILE:-}" ]]; then
+  echo "::error::attestation-file does not exist at ${ATTESTATION_FILE}"
+  exit 1
+fi
+
+IN_TOTO_JSON=$(cat "${ATTESTATION_FILE}")
+
+SUBJECT_COUNT=$(jq -r '.subject | if type == "array" then length else error("not an array") end' <<<"${IN_TOTO_JSON}")
+if (( SUBJECT_COUNT != 1 )); then
+  echo "::error::Unexpected number of attestation subjects: ${SUBJECT_COUNT}"
+  exit 1
+fi
+
+SUBJECT_JSON=$(jq '.subject[0]' <<<"${IN_TOTO_JSON}")
+SUBJECT_NAME=$(jq -r '.name' <<<"${SUBJECT_JSON}")
+SUBJECT_DIGEST=$(jq -r '.digest.sha256' <<<"${SUBJECT_JSON}")
+
+PREDICATE_TYPE=$(jq -r '.predicate_type' <<<"${IN_TOTO_JSON}")
+if [[ "${PREDICATE_TYPE}" != 'https://slsa.dev/provenance/v1' ]]; then
+  echo "::error::Unexpected predicate type: ${PREDICATE_TYPE}"
+  exit 1
+fi
+
+PREDICATE=$(jq '.predicate' <<<"${IN_TOTO_JSON}")
+PREDICATE_FILE=$(mktemp --tmpdir="${RUNNER_TEMP}" --suffix='.slsa.json')
+printf '%s' "${PREDICATE}" > "${PREDICATE_FILE}"
+
+echo "subject-name=${SUBJECT_NAME}" >> "${GITHUB_OUTPUT}"
+echo "subject-digest=sha256:${SUBJECT_DIGEST}" >> "${GITHUB_OUTPUT}"
+echo "predicate-file=${PREDICATE_FILE}" >> "${GITHUB_OUTPUT}"
